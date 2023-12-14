997bf663f4e8c66f564ff492b9ca61661eb174bb
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2023-12-14 08:50:36 +0000

    Limit dataset to a smaller one when using development env.

--- models/staging/staging_orders.sql
+++ models/staging/staging_orders.sql
@@ -3,8 +3,10 @@ WITH orders AS (
         *
     FROM {{ source("nettikauppasimulaattori_data_warehouse", "import_orders") }}
 
-    WHERE   id IS NOT NULL
-        AND order_placed IS NOT NULL
+    WHERE id IS NOT NULL AND order_placed IS NOT NULL
+    {% if target.name == 'dev'%} /* Limit dataset when in development environment. */
+    AND order_placed BETWEEN DATE_SUB(CURRENT_DATE("UTC"), INTERVAL 7 DAY) AND CURRENT_DATE("UTC")
+    {% endif %}
 )
 
 SELECT * FROM orders
